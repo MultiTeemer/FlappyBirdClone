8ed73b1525fe3d2b63db7d3fe1cc8461521e6747
monoklid <multiteemer@gmail.com>
2017-05-02 23:24:26 +1000

    Implement just failed state

--- FlappyBirdClone/JustFailedState.cpp
+++ FlappyBirdClone/JustFailedState.cpp
@@ -9,10 +9,28 @@ namespace FlappyBirdClone
 
 	void GameScreen::JustFailedState::Update(float delta)
 	{
-		screen.SetState<FailedState>();
+		const float zeroTolerance = 1e-6;
+
+		screen.gameWorld.Update(delta);
+
+		const auto b = screen.gameWorld.player.body;
+		const auto isPlayerIdle = abs(b->GetAngularVelocity()) < zeroTolerance
+			&& abs(b->GetLinearVelocity().Length()) < zeroTolerance;
+		if (isPlayerIdle) {
+			screen.SetState<FailedState>();
+		}
 	}
 
 	void GameScreen::JustFailedState::ProcessEvent(sf::Event& event) {}
 
-	void GameScreen::JustFailedState::OnSet() {}
+	void GameScreen::JustFailedState::OnSet()
+	{
+		screen.gameWorld.player.body->GetFixtureList()->SetDensity(5);
+		screen.gameWorld.player.body->GetFixtureList()->SetFriction(1);
+
+		screen.Ui.Get<sfg::Label>(GameScreen::scoreLabelId)->Show(true);
+
+		screen.Ui.Get<sfg::Label>(GameScreen::gamePausedLabelId)->Show(false);
+		screen.Ui.Get<sfg::Window>(GameScreen::recapWindowId)->Show(false);
+	}
 }
